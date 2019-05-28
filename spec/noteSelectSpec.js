4ed3248f3910a8f50b55df74db3eee91555a1fa7
Bill Muxworthy <bill_muxworthy@hotmail.com>
2019-05-28 16:42:43 +0100

    Select answers at random

--- spec/noteSelectSpec.js
+++ spec/noteSelectSpec.js
@@ -11,6 +11,12 @@ describe('NoteSelect', function() {
   it('should start with note list', function() {
     expect(noteSelect.list).toEqual([
       'A', 'A#', 'B', 'C', 'C#', 'D#', 'E', 'F', 'F#', 'G', 'G#'
-    ])
+    ]);
+  });
+
+  it('should randomly select note to be answer', function() {
+    for(var i = 0; i < 10; i++) {
+      expect(noteSelect.list).toContain(noteSelect.selectAnswer());
+    };
   });
 });
--- src/noteSelect.js
+++ src/noteSelect.js
@@ -2,7 +2,10 @@ function NoteSelect() {
   this.list = [
     'A', 'A#', 'B', 'C', 'C#', 'D#', 'E', 'F', 'F#', 'G', 'G#'
   ];
-  
+};
+
+NoteSelect.prototype.selectAnswer = function() {
+  return this.list[Math.floor(Math.random() * 12)]
 };
 
 module.exports = NoteSelect;
