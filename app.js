96fe87aa65eea4e0790f40837483e779f9b45d4f
Bill Muxworthy <bill_muxworthy@hotmail.com>
2019-06-24 20:15:42 +0100

    Add answer event listn

--- app.js
+++ app.js
@@ -2,3 +2,7 @@ function startWindowVanish() {
   document.getElementById('start_window').style.display = 'none';
   document.getElementById('quiz').style.display = 'block';
 }
+
+function getAnswer(answer) {
+  document.getElementById('answer').innerHTML = answer;
+}
